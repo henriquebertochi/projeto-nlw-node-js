3d1899e1c4fdc082fac00d3d19cedf669dd33374
Henrique Bertochi Grigol <henriquebg.bg@outlook.com>
2024-05-23 15:52:06 -0300

    começando a aula 03

--- src/routes/get-attendee-badge.ts
+++ src/routes/get-attendee-badge.ts
@@ -35,6 +35,17 @@ export async function getAttendeeBadge(app: FastifyInstance) {
                 throw new Error('Attendee not found.')
             }
 
-            return reply.send({ attendee })
+            const baseURL = `${request.protocol}://${request.hostname}`
+            
+            const checkInURL = new URL(`/attendees/${attendeeId}/check-in`, baseURL)
+
+            return reply.send({ 
+                badge: {
+                    name: attendee.name,
+                    email: attendee.email,
+                    eventTitle: attendee.event.title,
+                    checkInURL: checkInURL.toString(),
+                }
+            })
         })
 }
